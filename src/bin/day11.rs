3b1409b2a472ba20f133446d47a8827249479ee1
Pieter Bellens <pbellens@gmail.com>
2022-01-12 20:12:02 +0100

    Fix day 11 part 1

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,37 +1,61 @@
-const NEXT: [(isize, isize); 8] = [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)];
-const SIZE: usize = 10;
+use itertools::Itertools;
+use anyhow::Result;
 
-pub fn main() {
-    let mut m = include_bytes!("../../data/day11.input")
-        .split(|&b| b == b'\n')
-        .map(|l| l.to_vec())
-        .collect::<Vec<_>>();
 
-    println!(
-        "{}",
-        (0..100).fold(0, |acc, _| {
-            m.iter_mut()
-                .for_each(|row| row.iter_mut().for_each(|cell| *cell += 1));
-            acc + (0..SIZE)
-                .flat_map(|y| (0..SIZE).map(move |x| (x, y)))
-                .fold(0, |acc, (x, y)| {
-                    acc + (m[y][x] > b'9').then(|| flash(&mut m, x, y)).unwrap_or(0)
-                })
-        }),
-    );
+fn neighbors(x: usize, y: usize, m: usize, n: usize) -> Vec<(usize, usize)> {
+    let sx: i32 = x.try_into().unwrap();
+    let sy: i32 = y.try_into().unwrap();
+    let sm: i32 = m.try_into().unwrap();
+    let sn: i32 = n.try_into().unwrap();
+
+    vec![(-1,-1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1) ]
+        .into_iter()
+        .map(|(i, j)| (sx + i, sy + j))
+        .filter(|&(x,y)| x >= 0 && x < sm && y >= 0 && y < sn)
+        .map(|(x, y)| (x.try_into().unwrap(), y.try_into().unwrap()))
+        .collect_vec()
 }
 
-fn flash(map: &mut Vec<Vec<u8>>, x: usize, y: usize) -> usize {
-    map[y][x] = b'0';
-    NEXT.iter()
-        .map(|(xx, yy)| ((x as isize + xx) as usize, (y as isize + yy) as usize))
-        .fold(1, |acc, (x, y)| {
-            match map.get_mut(y).and_then(|l| l.get_mut(x)) {
-                Some(cell) if *cell > b'0' => {
-                    *cell += 1;
-                    acc + (*cell > b'9').then(|| flash(map, x, y)).unwrap_or(0)
-                }
-                _ => acc,
-            }
-        })
+fn flash(squids:&mut Vec<u32>, x: usize, y: usize, m: usize, n: usize) -> usize {
+    squids[x * n + y] = 0;
+    neighbors(x, y, m, n)
+        .into_iter()
+        .fold(
+            1,
+            |acc, (nx, ny)| 
+                match squids.get_mut(nx * n + ny) {
+                    Some(s) if *s > 0 => {
+                        *s += 1;
+                        acc + (*s > 9).then(|| flash(squids, nx, ny, m, n)).unwrap_or(0)
+                    },
+                    _ => acc,
+                })
+}
+
+
+fn main() -> Result<()> {
+    const M: usize = 10;
+    const N: usize = 10;
+    
+    let mut squids = include_str!("../../data/day11.input")
+        .lines()
+        .flat_map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect_vec())
+        .collect_vec();
+
+    let res = (0..100).fold(
+        0,
+        |acc, _| { 
+            squids.iter_mut().for_each(|s| { *s += 1; } ); 
+            acc + (0..M)
+                .flat_map(move |x| (0..N).map(move |y| (x,y)))
+                .fold(
+                    0,
+                    |acc, (x, y)| { 
+                        acc + (squids[x * N + y] > 9).then(|| flash(&mut squids, x, y, M, N)).unwrap_or(0)
+                    })
+        });
+        
+    println!("{}", res);
+
+    Ok(())
 }
