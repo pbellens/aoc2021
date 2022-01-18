5b539e9fbf078e2fc36033b0ee4c87656e9b00c7
Pieter Bellens <pbellens@gmail.com>
2022-01-18 21:56:18 +0100

    Fixing up day 12.

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -2,27 +2,42 @@ use itertools::Itertools;
 use std::collections::BTreeMap;
 use anyhow::Result;
 
+
 type Ids = BTreeMap<&'static str, usize>;
+type Edges = BTreeMap<usize, Vec<usize>>;
 
 fn main() -> Result<()> {
-    let insert_id = |mut c: Ids, id| {
+    let insert_id = |c: &mut Ids, id| {
         match c.get(id) {
-            Some(_) => c,
+            Some(idx) => *idx, 
             None => { 
-                c.insert(id, c.len() + 2); 
-                c 
+                let idx = c.len();
+                c.insert(id, idx);
+                idx
             }
         }
     };
 
-    let insert_ids = |mut c: Ids, id1, id2| insert_id(insert_id(c, id1), id2);
+    let insert_edge = |mut c: Edges, a, b| {
+            let entry = c.entry(a).or_insert_with(|| Vec::with_capacity(8));
+            (b != 0).then(|| entry.push(b));
+            c
+        };
 
     let ids = include_str!("../../data/day12.input")
         .lines()
         .fold(
-            BTreeMap::from([("start", 0), ("end", 1)]),
-            |acc, l| { 
-                l.split_once('-').map(|(b, e)| { insert_ids(acc, b, e) }).unwrap()
+            (
+                BTreeMap::from([("start", 0), ("end", 1)]),
+                Edges::new()
+            ),
+            |mut acc, l| { 
+                let (b, e) = l.split_once('-').unwrap();
+                let (bi, be) = (insert_id(&mut acc.0, b), insert_id(&mut acc.0, e));
+                (
+                    acc.0,
+                    insert_edge(acc.1, bi, be)
+                )
             });
 
     println!("{:?}", ids);
