1d3a243ce00d58238a109d8827a636abd5d3f26d
Pieter Bellens <pbellens@gmail.com>
2022-01-20 22:29:03 +0100

    Add day 12

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,18 +1,30 @@
 use itertools::Itertools;
 use std::collections::BTreeMap;
+use std::collections::HashSet;
 use anyhow::Result;
 
 
-type Ids = BTreeMap<&'static str, usize>;
+#[derive(Debug, Clone, Copy)]
+struct Cave {
+    idx: usize,
+    small: bool,
+}
+
+
+type Ids = BTreeMap<&'static str, Cave>;
 type Edges = BTreeMap<usize, Vec<usize>>;
 
 fn main() -> Result<()> {
     let insert_id = |c: &mut Ids, id| {
         match c.get(id) {
-            Some(idx) => *idx, 
+            Some(cave) => cave.idx,
             None => { 
                 let idx = c.len();
-                c.insert(id, idx);
+                c.insert(
+                    id, 
+                    Cave { 
+                        idx: idx, 
+                        small: id.chars().all(|c| c.is_lowercase()) });
                 idx
             }
         }
@@ -24,11 +36,11 @@ fn main() -> Result<()> {
             c
         };
 
-    let ids = include_str!("../../data/day12.input")
+    let (ids, edges) = include_str!("../../data/day12.input")
         .lines()
         .fold(
             (
-                BTreeMap::from([("start", 0), ("end", 1)]),
+                BTreeMap::from([("start", Cave { idx: 0, small: true }), ("end", Cave { idx: 1, small: false })]),
                 Edges::new()
             ),
             |mut acc, l| { 
@@ -36,11 +48,82 @@ fn main() -> Result<()> {
                 let (bi, be) = (insert_id(&mut acc.0, b), insert_id(&mut acc.0, e));
                 (
                     acc.0,
-                    insert_edge(acc.1, bi, be)
+                    insert_edge(insert_edge(acc.1, bi, be), be, bi)
                 )
             });
 
-    println!("{:?}", ids);
+    let smalls: HashSet<usize> = ids
+        .values()
+        .filter(|c| c.small)
+        .map(|c| c.idx)
+        .collect();
+
+    println!("ids: {:?}", ids);
+    println!("edges: {:?}", edges);
+
+    {
+        let mut paths = vec![];
+        let mut cand = vec![vec![0 as usize]];
+        while let Some(cs) = cand.pop() {
+            let last = cs.last().unwrap();
+            
+            if last == &1 {
+                paths.push(cs.clone());
+                continue;
+            }
+            
+            let visited : HashSet<usize> = HashSet::from_iter(cs.clone().into_iter())
+                .intersection(&smalls)
+                .cloned()
+                .collect();
+            for e in HashSet::from_iter(edges[&last].clone().into_iter())
+                .difference(&visited)
+            {
+                let mut freshpath = cs.clone();
+                freshpath.push(*e);
+                cand.push(freshpath);
+
+            }
+        }
+
+        println!("Solution part 1: {:?}", paths.len());
+    }
+
+    {
+        let mut paths = vec![];
+        let mut cand = vec![vec![0 as usize]];
+        while let Some(cs) = cand.pop() {
+            let last = cs.last().unwrap();
+            
+            if last == &1 {
+                paths.push(cs.clone());
+                continue;
+            }
+
+            let mut m : BTreeMap<usize, usize> = BTreeMap::new();
+            for n in cs.iter() {
+                if smalls.contains(n) {
+                    *m.entry(*n).or_insert(0) += 1;
+                }
+            }
+            let smalled = m.iter().any(|(k, v)| v == &2);
+
+            for e in edges[&last].clone().into_iter()
+            {
+                let count = m.get(&e).unwrap_or(&0);
+                if e != 0 
+                    && (!smalls.contains(&e) 
+                        || (count < &2 && !smalled)) {
+                    let mut freshpath = cs.clone();
+                    freshpath.push(e);
+                    cand.push(freshpath);
+                }
+
+            }
+        }
+
+        println!("Solution part 2: {:?}", paths.len());
+    }
 
     Ok(())
 }
